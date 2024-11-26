6e436c88a9ebdd81c8e64ec737910ee0d18323ad
aboelabbas Shahwan <abbas.shahwan@gmail.com>
2024-11-26 20:37:26 +0200

    revert using require

--- src/models/login.ts
+++ src/models/login.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import open from "open";
 import cors from "cors";
 import { setToken } from "../helper/auth";
 import logger from "../console/logger";
@@ -7,7 +8,6 @@ import logger from "../console/logger";
 const login = async (
     port: number = 4444,
 ): Promise<void> => {
-    const open = eval("require")('open');
     logger.log('Starting authentication process...');
     const authUrl = `https://partner.zid.sa/cli/authorized`;
     logger.log('Opening Zid Partner Dashboard in your default browser...');
